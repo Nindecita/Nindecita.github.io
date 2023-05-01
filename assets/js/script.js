417b11ad73765f1925c5321f7c9ce277505e6fa9
Nindecita <parancibiac@gmail.com>
2023-05-01 11:07:56 -0400

    se aplica responsive al proyecto

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,25 @@
-$(document).ready(function() { 
-
-
-  $("#paisaje").click( function() { 
+$(document).ready(function () {
+  $("#paisaje").click(function () {
     $("#paisaje-text").toggle("slow");
   });
 
-  $("#lago").click( function() { 
+  $("#lago").click(function () {
     $("#lago-text").toggle("slow");
   });
 
-  $("#desierto").click( function() { 
+  $("#desierto").click(function () {
     $("#desierto-text").toggle("slow");
   });
 
-  $("#bosque").click( function() { 
+  $("#bosque").click(function () {
     $("#bosque-text").toggle("slow");
   });
 
-  $("#completardatos").on('click', function() {
+  $("#completardatos").on("click", function () {
     alert("Los datos se ingresaron exitosamente");
-
   });
 
-  $("#enviarAsunto").tooltip({show: true})
+  $("#enviarAsunto").tooltip({ show: true });
 
-  $("#IngresarNombre").tooltip({show: true})
-
- 
-
+  $("#IngresarNombre").tooltip({ show: true });
 });
