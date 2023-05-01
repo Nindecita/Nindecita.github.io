96f9e349e7638b7c5063ebb8bb06f03b51e9fc9d
Nindecita <parancibiac@gmail.com>
2023-04-30 21:45:57 -0400

    Se agrega cdn jQuery y toggle a cards

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function() { 
+
+
+  $("#paisaje").click( function() { 
+    $("#paisaje-text").toggle("slow");
+  });
+
+  $("#lago").click( function() { 
+    $("#lago-text").toggle("slow");
+  });
+
+  $("#desierto").click( function() { 
+    $("#desierto-text").toggle("slow");
+  });
+
+  $("#bosque").click( function() { 
+    $("#bosque-text").toggle("slow");
+  });
+
+
+  });
+
+
+
